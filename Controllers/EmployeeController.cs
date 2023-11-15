58a7a864710458369b19fe683a79f68758670053
raiaman <amnrai8@gmail.com>
2023-11-15 19:17:56 +0530

    first commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmpModel emp = new EmpModel
+            {
+                EmpId = 100,
+                Name = "Akbar",
+                City = "Mum",
+                Address = "xyz"
+
+            };
+            return View(emp);
+        }
+    }
+}
---
+++ Models/EmpModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmpModel
+    {
+        public int EmpId { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+    }
+}
